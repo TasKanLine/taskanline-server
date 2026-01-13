28dcffdbf42b4bfb60d527b19d521361958251ec
Yernaz Kenesbekov <yernazkenesbekov24@gmail.com>
2026-01-13 16:24:41 +0500

    feat: обновить модели пользователей с использованием TIMESTAMP

--- src/models/users.py
+++ src/models/users.py
@@ -1,6 +1,7 @@
-from datetime import datetime, UTC
+from datetime import datetime, UTC, date
 
-from sqlalchemy import Column, String, Boolean, ForeignKey, Date, DateTime
+from sqlalchemy import Column, String, Boolean, ForeignKey, Date, TIMESTAMP
+from sqlalchemy.sql import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from database.session import Base
@@ -14,9 +15,9 @@ class User(Base):
     username: Mapped[str] = mapped_column(String(30), unique=True, index=True)
     password = Column(String)
     is_admin = Column(Boolean, default=False)
-    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now)
-    updated_at: Mapped[DateTime] = mapped_column(
-        DateTime, default=datetime.now, onupdate=datetime.now
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), default=func.now(), onupdate=func.now()
     )
 
 
@@ -27,11 +28,11 @@ class UserProfile(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     first_name: Mapped[str] = mapped_column(String(30))
     last_name: Mapped[str] = mapped_column(String(30))
-    birth_date: Mapped[Date] = mapped_column(Date, nullable=True)
+    birth_date: Mapped[date] = mapped_column(Date, nullable=True)
     phone_number: Mapped[str] = mapped_column(String(10), nullable=True)
     gender: Mapped[str] = mapped_column(String(10), nullable=True)
     avatar: Mapped[str] = mapped_column(String(255), nullable=True)
-    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now)
-    updated_at: Mapped[DateTime] = mapped_column(
-        DateTime, default=datetime.now, onupdate=datetime.now
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP(timezone=True), default=func.now(), onupdate=func.now()
     )
